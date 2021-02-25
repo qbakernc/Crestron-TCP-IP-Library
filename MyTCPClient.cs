78047be49fb215dc8ad9a3bbb898af92761b5988
qbakernc <79540225+qbakernc@users.noreply.github.com>
2021-02-24 20:48:48 -0500

    Add files via upload

---
+++ MyTCPClient.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.Diagnostics;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.UI;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.CrestronSockets;
+
+namespace AisleTCPLibrary
+{
+    public class MyTCPClient
+    {
+        public EventHandler DataReceivedEvent;
+        private TCPClient tcpClient;
+        private string IpAddress;     // IpAddress.
+        private int port;             // Port Number.    
+        private int buffSize;         // Buffer Size.
+        private Byte[] sendBytes;     // Data to Send
+        private string rcvString;     // String received from the Server
+        private string delimiter;     // This is what we will be looking for in the response from the server. Lets us know when we gathered all of the data
+
+        public string RcvString
+        {
+            get { return rcvString; }
+            set { rcvString = value; }
+        }
+        public MyTCPClient(string IpAddress, int port, int buffSize, string delimiter)
+        {
+            this.IpAddress = IpAddress;
+            this.port = port;
+            this.buffSize = buffSize;
+            this.delimiter = delimiter;
+
+            tcpClient = new TCPClient(this.IpAddress, this.port, this.buffSize);
+            tcpClient.SocketStatusChange += new TCPClientSocketStatusChangeEventHandler(masterConfigList_SocketStatusChange);
+        }
+
+        public void ConnectToServer(Byte[] sendBytes)
+        {
+            this.sendBytes = sendBytes;
+
+            try {
+                /* ClientConnectCallback is called once the client connects succesfully. */
+                SocketErrorCodes error = tcpClient.ConnectToServerAsync(ClientConnectCallback);
+            }
+            catch (Exception e) {
+                CrestronConsole.PrintLine("Error connecting to server: " + e.Message);
+            }
+        }
+
+        private void ClientConnectCallback(TCPClient myTCPClient)
+        {
+            if (myTCPClient.ClientStatus == SocketStatus.SOCKET_STATUS_CONNECTED) {
+                /* Once connected, begin waiting for packets from the server.
+                 * This call to ReceiveDataAsync is necessary to receive the FIN packet from the server in the event
+                 * that the TLS handshake fails and the connection cannot be made. If you do not call ReceiveDataAsync here,
+                 * the client will remain "connected" from its perspective, though no connection has been made. */
+                myTCPClient.ReceiveDataAsync(ClientReceiveCallback);
+                myTCPClient.SendData(sendBytes, sendBytes.Length);
+            } else {
+                CrestronConsole.PrintLine("clientConnectCallback: No connection could be made with the server.");
+            }
+        }
+
+        public void ClientReceiveCallback(TCPClient myTCPClient, int totalBytesRcv)
+        {
+            /* 0 or negative byte count indicates the connection has been closed. */
+            if (totalBytesRcv <= 0) {
+                CrestronConsole.PrintLine("clientReceiveCallback: Could not receive message- connection closed");
+            } else {
+                try {
+                    byte[] rcvBytes = new byte[totalBytesRcv];
+                    Array.Copy(myTCPClient.IncomingDataBuffer, rcvBytes, totalBytesRcv);
+
+                    /* Gathers the data into a string variable. */
+                    rcvString += Encoding.UTF8.GetString(rcvBytes, 0, totalBytesRcv);
+
+                    /* Event will be called if the delimiter is found in the string. Lets us know we gathered all of the data. */
+                    if (DataReceivedEvent != null && rcvString.IndexOf(delimiter) >= 0) {
+                        DataReceivedEvent.Invoke(this, EventArgs.Empty);
+                        rcvString = "";
+                    } else {
+                        myTCPClient.ReceiveDataAsync(ClientReceiveCallback);
+                    }
+                }
+                catch (Exception e) {
+                    CrestronConsole.PrintLine("Exception in clientReceiveCallback: " + e.Message);
+                }
+            }
+        }
+
+        public void Disconnect()
+        {
+            /* Disconnect from the server once we gathered all of the data. */
+            tcpClient.DisconnectFromServer();
+        }
+
+        public void masterConfigList_SocketStatusChange(TCPClient myTCPClient, SocketStatus clientSocketStatus)
+        {
+            CrestronConsole.PrintLine("Socket State: " + clientSocketStatus);
+        }
+    }
+}
---
+++ MyTCPServer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.Diagnostics;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.UI;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.CrestronSockets;
+
+namespace AisleTCPLibrary
+{
+    public class MyTCPServer
+    {
+        public EventHandler DataReceivedEvent;
+        TCPServer tcpServer;
+        private string addressToAccept = "0.0.0.0"; // Accepts Connection from any client.
+        private int port;                           // Port Number.    
+        private int buffSize;                       // Buffer Size.
+        private int maxConnections;                 // Maximum number of connected Clients.
+        private string rcvString;                   // String received from the client.
+        private string delimiter;                   // Lets us know when we gathered all of the data
+
+        public string RcvString
+        {
+            get { return rcvString; }
+            set { rcvString = value; }
+        }
+
+        public MyTCPServer(int port, int buffSize, int maxConnections, string delimiter)
+        {
+            this.port = port;
+            this.buffSize = buffSize;
+            this.maxConnections = maxConnections;
+            this.delimiter = delimiter;
+
+            tcpServer = new TCPServer(addressToAccept, port, buffSize, EthernetAdapterType.EthernetLANAdapter, maxConnections);
+            tcpServer.SocketStatusChange += new TCPServerSocketStatusChangeEventHandler(server_SocketStatusChange);
+        }
+
+        public void ListenForConnection()
+        {
+            SocketErrorCodes error = tcpServer.WaitForConnectionAsync(ServerConnectedCallback);
+            CrestronConsole.PrintLine("WaitForConnectionAsync return: " + error);
+        }
+
+        private void ServerConnectedCallback(TCPServer myTCPServer, uint clientIndex)
+        {
+            if (clientIndex != 0) {
+                CrestronConsole.PrintLine("Server listening on port #" + myTCPServer.PortNumber);
+                CrestronConsole.PrintLine("Connected: client #" + clientIndex);
+
+                /* Starts listeing for the specific client index. */
+                myTCPServer.ReceiveDataAsync(clientIndex, ServerDataReceivedCallback);
+
+                if (myTCPServer.MaxNumberOfClientSupported == myTCPServer.NumberOfClientsConnected) {
+                    CrestronConsole.PrintLine("Client limit reached. Server will stop listening.");
+                    myTCPServer.Stop();
+                    CrestronConsole.PrintLine("Server State: " + myTCPServer.State);
+                }
+            }
+                /* A clientIndex of 0 could mean that the server is no longer listening, or that the TLS handshake failed when a client tried to connect.
+                 * In the case of a TLS handshake failure, wait for another connection so that other clients can still connect. */
+            else {
+                CrestronConsole.Print("Error in ServerConnectedCallback: ");
+                if ((myTCPServer.State & ServerState.SERVER_NOT_LISTENING) > 0) {
+                    CrestronConsole.PrintLine("Server is no longer listening.");
+                } else {
+                    CrestronConsole.PrintLine("Unable to make connection with client.");
+                    /* This connection failed, but keep waiting for another. */
+                    myTCPServer.WaitForConnectionAsync(ServerConnectedCallback);
+                }
+            }
+
+        }
+
+        private void ServerDataReceivedCallback(TCPServer myTCPServer, uint clientIndex, int totalBytesRcv)
+        {
+            /* 0 or negative byte count indicates the connection has been closed. */
+            if (totalBytesRcv <= 0) {
+                CrestronConsole.PrintLine("Error: server's connection with client " + clientIndex + " has been closed.");
+                myTCPServer.Disconnect(clientIndex);
+
+                /* If server is not listening because of max number of clients, It will start listening again because a client disconnected. */
+                if ((myTCPServer.State & ServerState.SERVER_NOT_LISTENING) > 0)
+                    myTCPServer.WaitForConnectionAsync(ServerConnectedCallback);
+            } else {
+                byte[] rcvBytes = new byte[totalBytesRcv];
+                Array.Copy(myTCPServer.GetIncomingDataBufferForSpecificClient(clientIndex), rcvBytes, totalBytesRcv);
+                rcvString += Encoding.UTF8.GetString(rcvBytes, 0, totalBytesRcv);
+
+                /* Use this block of code to see the data comming into the server on the CrestronConsole*/
+                //CrestronConsole.PrintLine("----------- incoming message -----------\n\r" + rcvString + "\n\r---------- end of message ----------"); 
+
+                /* Event will be called if the expected delimiter is found in the string. Clears the rcvString. */
+                if (DataReceivedEvent != null && rcvString.IndexOf(delimiter) >= 0) {
+                    DataReceivedEvent.Invoke(this, EventArgs.Empty);
+                    rcvString = "";
+                }
+
+                /* Begin waiting for another message from that same client. */
+                myTCPServer.ReceiveDataAsync(clientIndex, ServerDataReceivedCallback);
+            }
+        }
+
+        private void server_SocketStatusChange(TCPServer myTCPServer, uint clientIndex, SocketStatus socketStatus)
+        {
+            /* Lets us know if the connection was succesfull or not. It also lets us know which specific client had a succesfull connection. */
+            if (socketStatus == SocketStatus.SOCKET_STATUS_CONNECTED) {
+                CrestronConsole.PrintLine("Client #" + clientIndex + "connected");
+            } else {
+                CrestronConsole.PrintLine("Client #" + clientIndex + ": " + socketStatus + ".");
+            }
+        }
+    }
+}
+
